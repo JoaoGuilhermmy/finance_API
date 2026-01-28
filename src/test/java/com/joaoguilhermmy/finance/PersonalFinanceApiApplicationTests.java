b3acb57e300b067da067e31c6582804f89ce7228
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-27 22:58:44 -0300

    Initial project

---
+++ src/test/java/com/joaoguilhermmy/finance/PersonalFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoguilhermmy.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
