a3b23bade63dec18d6a0e9d18f5c629dc348b27c
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-29 23:23:09 -0300

    Writing endpoint documentation and implementing Swagger.

--- src/main/java/com/joaoguilhermmy/finance/resources/ExpenseResource.java
+++ src/main/java/com/joaoguilhermmy/finance/resources/ExpenseResource.java
@@ -18,25 +18,45 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.joaoguilhermmy.finance.entities.Expense;
 import com.joaoguilhermmy.finance.services.ExpenseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/expenses")
+@Tag(name = "Expense Management", description = "Endpoints for managing expenses")
 public class ExpenseResource {
 
     @Autowired
     private ExpenseService service;
 
+    @Operation(summary = "Find all expenses", description = "Retrieves a list of all registered expenses.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     @GetMapping
     public ResponseEntity<List<Expense>> findAll() {
         List<Expense> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(summary = "Find expense by id", description = "Retrieves an expense by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "404", description = "Expense not found"),
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<Expense> findById(@PathVariable Integer id) {
         Expense obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @Operation(summary = "Create a new expense", description = "Creates a new expense in the database and returns the created object with its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Expense created")
+    })
     @PostMapping
     public ResponseEntity<Expense> insert(@RequestBody Expense obj) {
         obj = service.insert(obj);
@@ -44,12 +64,23 @@ public class ExpenseResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @Operation(summary = "Delete an expense", description = "Deletes an expense by their unique identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Expense deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Expense not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error (Integrity violation)")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Update expense", description = "Updates an existing expense record.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Expense Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Expense not found"),
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<Expense> update(@PathVariable Integer id, @RequestBody Expense expense) {
         expense = service.update(id, expense);
--- src/main/java/com/joaoguilhermmy/finance/resources/UserResource.java
+++ src/main/java/com/joaoguilhermmy/finance/resources/UserResource.java
@@ -20,8 +20,14 @@ import com.joaoguilhermmy.finance.entities.User;
 import com.joaoguilhermmy.finance.services.ExpenseService;
 import com.joaoguilhermmy.finance.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User Management", description = "Endpoints for managing users")
 public class UserResource {
 
     @Autowired
@@ -30,24 +36,44 @@ public class UserResource {
     @Autowired
     private ExpenseService expenseService;
 
+    @Operation(summary = "Find all users", description = "Retrieves a list of all registered users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(summary = "Find user by id", description = "Retrieves a user by the id that was passed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 
+    @Operation(summary = "Delete a user", description = "Deletes a user by their unique identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error (Integrity violation)")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Create a new user", description = "Creates a new user in the database and returns the created object with its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created")
+    })
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
@@ -55,12 +81,22 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @Operation(summary = "Update user", description = "Updates data for an existing user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user) {
         user = service.update(id, user);
         return ResponseEntity.ok().body(user);
     }
 
+    @Operation(summary = "Get user expenses", description = "Returns all expenses for a given user by their id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+    })
     @GetMapping(value = "/{id}/expenses")
     public ResponseEntity<List<Expense>> findExpensesByUser(@PathVariable Integer id) {
         List<Expense> list = expenseService.findByUser(id);
